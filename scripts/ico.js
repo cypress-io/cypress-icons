b01d3e69cd31376fdb638a3455db61a59f85e512
Brian Mann <brian@backbonerails.com>
2017-09-20 14:17:34 -0400

    add 24 and 64 sized icons

--- scripts/ico.js
+++ scripts/ico.js
@@ -3,14 +3,14 @@ const toIco = require('to-ico')
 
 const files = [
 	fs.readFileSync('./src/icons/icon_16x16.png'),
+	fs.readFileSync('./src/icons/icon_24x24.png'),
 	fs.readFileSync('./src/icons/icon_32x32.png'),
 	fs.readFileSync('./src/icons/icon_48x48.png'),
+	fs.readFileSync('./src/icons/icon_64x64.png'),
 	fs.readFileSync('./src/icons/icon_128x128.png'),
 	fs.readFileSync('./src/icons/icon_256x256.png'),
 ]
 
-const sizes = [16, 32, 48, 128, 256]
-
-toIco(files, sizes).then(buf => {
+toIco(files).then(buf => {
 	fs.writeFileSync('./dist/icons/cypress.ico', buf)
 })
